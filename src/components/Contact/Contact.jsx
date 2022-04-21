515544299f6769844dcd561d15981dac7852e336
DJ <darrinjhansen@gmail.com>
2022-04-21 09:00:42 -0400

    Added Contact Page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,6 +3,18 @@ import './Contact.scss'
 
 export default function Contact() {
   return (
-    <div className="contact" id="contact">Contact</div>
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form>
+          <input type="text" placeholder='Email' />
+          <textarea placeholder='Message'></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
