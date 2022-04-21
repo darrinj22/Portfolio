852b7a366453eb670e6f36fd021ea03e09d2afde
DJ <darrinjhansen@gmail.com>
2022-04-21 10:15:18 -0400

    responsive about section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import './Contact.scss'
+import React from 'react';
+import './Contact.scss';
+import { useState } from 'react'; 
 
 export default function Contact() {
+  const [message, setMessage] = useState(false); 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true); 
+  }
+
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -9,10 +16,11 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder='Email' />
           <textarea placeholder='Message'></textarea>
           <button type="submit">Send</button>
+          {message && <span>Thanks, I will reply ASAP</span>}
         </form>
       </div>
     </div>
