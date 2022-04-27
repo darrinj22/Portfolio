b994d998d48a39f705afc7a3af2915363931fdd9
DJ <darrinjhansen@gmail.com>
2022-04-27 14:04:50 -0400

    finished simple about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -27,8 +27,13 @@ export default function About() {
       <div className='right'>
         <div className="wrapper">
           <h1>About Me.</h1>
-          <p>data to be displayed to the screen. I'm not really sure why I can't seem to figure out why these 
-            test cases look so wierd to me.
+          <p>Alexander Nox used to be one of the brightest scientists in the employ of Humbert Labs, the Frontier’s leading manufacturer of pesticide gases.
+             Nox worked day and night developing new formulas 
+            for the glut of pesticides needed to protect the crops that sustained the Outlands. 
+            But he soon realized that progress was being hampered by his testing methods. </p><p>He needed to test on more than just inert tissue: 
+            he needed living subjects.As he toiled in secret, Nox began to see the beauty in his creations and their ability to destroy anything they touched. 
+            But the head of Humbert Labs soon discovered his gruesome experiments, and their confrontation ended with the lab in flames. Now, competing under
+             the name Caustic, he puts his gaseous creations to work in the Apex Games and observes their effects with great interest.
           </p>
         </div>
         <a href="#resume">
